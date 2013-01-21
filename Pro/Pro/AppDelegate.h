f73b28e9e76ac00260dca2c651d90312951f88e5
Hieu Ha <hieuha@Hieus-Mac.local>
2013-01-21 20:07:17 +0700

    First commit

---
+++ Pro/Pro/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pro
+//
+//  Created by Hieu Ha on 1/21/13.
+//  Copyright (c) 2013 Hieu Ha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pro/Pro/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Pro
+//
+//  Created by Hieu Ha on 1/21/13.
+//  Copyright (c) 2013 Hieu Ha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
